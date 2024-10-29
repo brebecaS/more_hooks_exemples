8afed86b2cc0216d5351505952101e92a2f5f559
brebecaS <birleanu0rebeca@gmail.com>
2024-10-29 20:46:45 +0200

    pasii pt useContext

--- src/exemple/useContextExemple/ComponentaParinte.js
+++ src/exemple/useContextExemple/ComponentaParinte.js
@@ -4,6 +4,12 @@ import { useState, createContext } from "react";
 export const ThemeContext = createContext();
 export const FontSizeContext = createContext();
 
+//1. Creaza un context folosind createContext din react
+//2. Export contextul
+//3. Faci wrapping la componnetele care au nevoie de valaorea din context cu Context.Provider
+//4. Daugam valoarea in context  cu value={valoare}
+//5. In componentele care au nevoie de valoare din context folosim useContext apelat cu contextul importat
+
 const ComponentaParinte = () => {
   const [theme, setTheme] = useState("dark");
   const [fontSize, setFontSize] = useState(10);
